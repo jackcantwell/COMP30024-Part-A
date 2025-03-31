4e44eec948f242ddbc88fb9712e1ee4e763fde97
Thomas Eley <thomas-eley@hotmail.co.uk>
2025-03-31 04:57:01 +0000

    Eley Update 1

--- search/program.py
+++ search/program.py
@@ -8,7 +8,7 @@ import math
 
 # Possible directions that the red frog can move in
 DIRECTIONS = [Direction.Down, Direction.DownRight, Direction.DownLeft,
-              Direction.Left, Direction.Right]
+              Direction.Right, Direction.Left]
 
 # The heuristic used assumes the shortest possible path would be to consecutively
 # hop over blue monkeys all the way to the bottom row, this is an admissable
@@ -30,11 +30,11 @@ def is_goal(pos, board):
     
 # Get the number of the bottom row based on the board size
 def getBottomRow(board):
-    return len(set(coord.r for coord in board)) - 1
+    return max(set(coord.r for coord in board))
 
 # Get the number of the right row based on the board size
 def getRightRow(board):
-    return len(set(coord.c for coord in board)) - 1
+    return max(set(coord.c for coord in board))
 
 
 # Obtain the possible future nodes based on the current node
@@ -59,6 +59,7 @@ def get_neighbours(board, pos):
 # Either returns a None tuple if the move is invalid or a tuple of the next board 
 # and red frog position if the move is valid
 def apply_move(board, pos, direction):
+    #print(direction)
     nextBoard = board.copy()
 
     # The red frog is moving off the current pos, so delete the cell state
@@ -70,14 +71,13 @@ def apply_move(board, pos, direction):
     nextC = pos.c+direction.c
 
     # Check if the future position is within the boundaries of the board
-    if not (0 <= nextR < getRightRow(nextBoard)) or not (0 <= nextC < getBottomRow(nextBoard)):  
+    if not (0 <= nextR <= getRightRow(nextBoard)) or not (0 <= nextC <= getBottomRow(nextBoard)):  
         return (None, None)
     
     # Wrap the next position in a coordinate (given it is valid)
-    nextPos = Coord(pos.r+direction.r, pos.c+direction.c)
+    nextPos = Coord(nextR, nextC)
 
     # print(nextPos.r, nextPos.c)
-    # print(render_board(nextBoard, ansi=True))
 
     # Check if the next position is an empty CellState (e.g., not a lilypad nor blue frog)
     if nextPos not in nextBoard:
@@ -86,6 +86,8 @@ def apply_move(board, pos, direction):
     # If the next position is a lilypad, it is a valid move
     elif nextBoard[nextPos] == CellState.LILY_PAD:
         nextBoard[nextPos] = CellState.RED
+        #print("OPTION")
+        #print(render_board(nextBoard, ansi=True))
         return (nextBoard, nextPos)
     
     # If the next position is a blue frog, check if the red frog can jump over it 
@@ -96,7 +98,9 @@ def apply_move(board, pos, direction):
             # Simply recall this function and check the CellState after the frog
             # if there is a lilypad, it will just return that position, if not
             # it returns None
+            #print("Blue Check")
             return apply_move(nextBoard, nextPos, direction)
+    return (None, None)
 
 def search(
     board: dict[Coord, CellState]
@@ -125,12 +129,12 @@ def search(
 
     # PLAN:
     # We need a priority queue for the search. Each time the node
-    # is popped, we expand the possible solutions (there can only be 5 at most)
-    # and run them through the heuristic, then respectively place them in 
-    # the priority queue in order. the value is based on the amount of moves
-    # taken to get there + the shortest possible amount of moves that could
-    # result in a solution (aka if the frog hopped its way over frogs to the 
-    # bottom row)
+    # is popped, we expand the possible solutions (there can only be 5 at most) 
+    # (branching factor = 5) and run them through the heuristic, 
+    # then respectively place them in the priority queue in order. the value 
+    # is based on the amount of moves taken to get there + the shortest 
+    # possible amount of moves that could result in a solution (heuristic)
+    # (aka if the frog hopped its way over frogs to the bottom row)
 
     # Implement PQ using min-heap?
 
@@ -170,6 +174,7 @@ def search(
     initialCost = initialSteps + initialHeuristic
     initialPath = []
 
+    nodeCounter = 1
 
     # Initialise the Priority Queue (PQ)
     PQ = []
@@ -177,16 +182,35 @@ def search(
     # Python's heapq automatically organizes based on the first element which is the cost
     # The PQ contains: the cost (steps + heuristic), the steps to get there,
     # the red frog position, the board at that state, and the path to get there
-    heapq.heappush(PQ, (initialCost, initialSteps, initialPos, 
+
+    heapq.heappush(PQ, (initialCost, nodeCounter, initialSteps, initialPos, 
                         board, initialPath))
     
+    #print(PQ)
+    
     # Store the nodes visited to ignore duplicates
-    # idk if this is needed maybe this is the problem
-    # visited = set()
+    # Given that from at a node, the heuristic is unchanging
+    # (i.e if you visit the same node twice - the heuristic is the same value)
+    # We assume that the first time a node is visited will be the cheapest
+    # path TO that node - and therefore the TOTAL cost is minimised through 
+    # that node the FIRST time we expand it... hence, there is no need to explore
+    # a given spot on the board twice.
+
+    visited = set(initialPos)
 
     while PQ:
         # Expand the node, 
-        cost, steps, pos, board, path = heapq.heappop(PQ)
+        cost, counter, steps, pos, board, path = heapq.heappop(PQ)
+        #print(path)
+        #print("CHOSEN OPTION")
+        print(render_board(board, ansi=True))
+        
+        # if the node being expanded has already been visited
+        # no need to unnecessarily repeat steps.
+        if (pos in visited):
+            continue
+        else:
+            visited.add(pos)
 
         # Is this node at the bottom row?
         if is_goal(pos, board):
@@ -201,18 +225,18 @@ def search(
             nextCost = nextSteps + heuristic(nextPos.r, board)
 
             # Add this to the PQ as generated nodes
-            heapq.heappush(PQ, (nextCost, nextSteps, nextPos, nextBoard, 
+            ## ISSUE HERE - When heapq adds next value to the PQ, nextCost is used 
+            ## If nextCost is equal, it moves on, comparing then to nextSteps, and so on
+            ## This is not an issue until we try to compare boards - in which case 
+            ## you are NOT able to compare two dictionaries - and we get an error
+            ## Proposed solution, have a "nodeNumber" variable (like an ID - unique
+            ## to every node) which is the second variable after "nextCost"
+            ## this way we will simply put the nodes in order based on when they appear
+            ## **UNLESS THE COSTS ARE DIFFERENT
+
+            nodeCounter += 1
+            heapq.heappush(PQ, (nextCost, nodeCounter, nextSteps, nextPos, nextBoard, 
                                 path + [move]))
     # If PQ is empty it means no solution
     return None
 
-
-    # ignore this:
-    return [
-        MoveAction(Coord(0, 5), [Direction.Down]),
-        MoveAction(Coord(1, 5), [Direction.DownLeft]),
-        MoveAction(Coord(3, 3), [Direction.Left]),
-        MoveAction(Coord(3, 2), [Direction.Down, Direction.Right]),
-        MoveAction(Coord(5, 4), [Direction.Down]),
-        MoveAction(Coord(6, 4), [Direction.Down]),
-    ]
